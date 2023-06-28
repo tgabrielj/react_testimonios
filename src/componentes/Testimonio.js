f73b05340e77ec299751261edf9eff6ef01e3c8d
gjuncos <testingabriel@gmail.com>
2023-06-28 12:03:25 -0300

    first commit

---
+++ src/componentes/Testimonio.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+function Testimonio(){
+    return(
+        <div className='contenedor-testimonio'>
+            <img
+                className='imagen-testimonio'
+                src={require('../imagenes/testimonio_frente.png')}
+                alt='Foto de frente'/>
+            <div className='contenedor-texto-testimonio'>
+                <p className='nombre-testimonio'>Emma Bostian en Suecia</p>
+                <p className='cargo-testimonio'>Ingenieria de software en spotify</p>
+                <p className='texto-testimonio'>Aca va una descripcion</p>
+            </div>
+        </div>
+    )
+}
+
+export default Testimonio;
